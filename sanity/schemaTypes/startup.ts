f5f3f0804cda95db945d57c70ad14d7a5202397a
dhunanyan <davit.hunanyan@me.com>
2024-12-10 00:16:59 +0100

    feat: add typegen for sanity

--- sanity/schemaTypes/startup.ts
+++ sanity/schemaTypes/startup.ts
@@ -19,9 +19,7 @@ export const startup = defineType({
     defineField({
       name: "author",
       type: "reference",
-      to: {
-        type: "author",
-      },
+      to: { type: "author" },
     }),
     defineField({
       name: "views",
