be4d52d637d472c6594885608e4129af039d2209
dhunanyan <davit.hunanyan@me.com>
2024-12-09 01:59:43 +0100

    feat: init shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,38 @@ const config: Config = {
           "100": "#F7F7F7",
           DEFAULT: "#FFFFFF",
         },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
       },
       fontFamily: {
         "work-sans": ["var(--font-work-sans)"],
@@ -39,9 +71,9 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)",
       },
       boxShadow: {
-        100: "2px 2px 0px 0px rgb(0, 0, 0)",
-        200: "2px 2px 0px 2px rgb(0, 0, 0)",
-        300: "2px 2px 0px 2px rgb(238, 43, 105)",
+        "100": "2px 2px 0px 0px rgb(0, 0, 0)",
+        "200": "2px 2px 0px 2px rgb(0, 0, 0)",
+        "300": "2px 2px 0px 2px rgb(238, 43, 105)",
       },
     },
   },
