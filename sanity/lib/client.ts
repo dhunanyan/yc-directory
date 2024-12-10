69c041ac57011366616e691f5c9c8beabbf8b60d
dhunanyan <davit.hunanyan@me.com>
2024-12-10 05:03:39 +0100

    feat: switch from ISR to real time data display

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  useCdn: false, // When set to true - user needs to wait for 60sec to see the updated data if new data is added
 });
