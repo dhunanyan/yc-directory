acf5f6c9cfa3d25e1b27c3f1726ede0e093e04ac
dhunanyan <davit.hunanyan@me.com>
2024-12-13 21:53:17 +0100

    fix: resolve issues with toast styles and exclude playlists from test results

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`
-*[_type == 'startup' && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+*[_type == "startup" && ((defined(slug.current) && !defined($search)) || (title match $search || category match $search || author->name match $search))] | order(_createdAt desc) {
   _id,
   title,
   slug,
@@ -19,7 +19,7 @@ export const STARTUPS_QUERY = defineQuery(`
 }`);
 
 export const STARTUP_BY_ID_QUERY = defineQuery(`
-*[_type == 'startup' && _id == $id][0] {
+*[_type == "startup" && _id == $id][0] {
   _id,
   title,
   slug,
@@ -39,13 +39,13 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`
 }`);
 
 export const STARTUP_VIEWS_QUERY = defineQuery(`
-*[_type == 'startup' && _id == $id][0] {
+*[_type == "startup" && _id == $id][0] {
   _id,
   views
 }`);
 
 export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
-*[_type == 'author' && id == $id][0] {
+*[_type == "author" && id == $id][0] {
   _id,
   id,
   name,
@@ -56,7 +56,7 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
 }`);
 
 export const AUTHOR_BY_ID_QUERY = defineQuery(`
-*[_type == 'author' && _id == $id][0] {
+*[_type == "author" && _id == $id][0] {
   _id,
   id,
   name,
@@ -67,7 +67,7 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
 }`);
 
 export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`
-*[_type == 'startup' && author._ref == $id] | order(_createdAt desc) {
+*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
   _id,
   title,
   slug,
